4b86b27e9ef9cca3627624a183a0b15538fd7030
nox-vixen <nox.vixen.0@gmail.com>
2026-07-08 13:22:20 +0530

    Add provider debug endpoint

--- src/routes/debug.ts
+++ src/routes/debug.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { cache } from "../cache";
+import { providerRegistry } from "../providers";
 
 const router = Router();
 
+router.get("/", async (_req, res) => {
+  res.json({
+    providers: await providerRegistry.health()
+  });
+});
+
 router.post("/cache/clear", async (_req, res) => {
   await cache.clear();
+
   res.json({
     success: true,
     message: "Cache cleared"
